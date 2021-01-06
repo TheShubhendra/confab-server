a0eb4e7fe6cf1a1eeb613abd011c93b275d9997a
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-01-06 17:47:51 +0530

    Add requirements

---
+++ server.py
@@ -0,0 +1,17 @@
+"""Main server file to handle HTTP API requests """
+import os
+import pyscopg2
+from flask import Flask, request
+
+
+TOKEN = os.environ.get("TOKEN")
+DATABASE_URL = os.environ.get("DATA")
+app = Flask(__name__)
+
+
+@app.route('/api/login',methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+    
