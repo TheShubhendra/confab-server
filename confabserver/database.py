b76e23ba050ec73d2469c02f11289541ce1b633d
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-01-06 17:34:55 +0530

    Create database module

---
+++ confabserver/database.py
@@ -0,0 +1,15 @@
+"""Database module"""
+import os
+import psycopg2
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+class ConfabDatabaseConnector:
+    
+    def __init__(self):
+        self.connection = psycopg2.connect(DATABASE_URL)
+        self.cursor = self.connection.cursor()
+    
+    def execute(self, sql, values=None):
+        self.cursor.execute(sql, values)
+    def commit(self):
+        self.connection.commit()
